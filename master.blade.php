6705bdf4b41dc439376e3b8bf1f4f7069940bc43
Bishwajeet kumar shah <38222738+bishwajeetk1628@users.noreply.github.com>
2019-04-12 12:16:26 +0530

    Add files via upload
    master file,view files

---
+++ master.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+<title>TreasureBox</title>
+<link rel="icon" href="icon.png" type="image/x-icon">
+<link href="https://cdnjs.cloudfare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+@yield('content')
+</body>
+</html>
